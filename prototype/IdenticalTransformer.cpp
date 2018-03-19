3d82d7037280e309c01c7af61d9f323f945dcb51
seanzw <seanyukigeek@gmail.com>
2018-03-18 18:27:24 -0700

    add memory base/offset and skeleton for identical transform

---
+++ prototype/IdenticalTransformer.cpp
@@ -0,0 +1,7 @@
+#include "IdenticalTransformer.h"
+
+IdenticalTransformer::IdenticalTransformer(DynamicTrace* _Trace)
+    : Trace(_Trace) {
+  // Simply generate the output data graph for gem5 to use.
+  
+}
