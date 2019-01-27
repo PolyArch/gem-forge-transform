f7d0ec3d5f08f8d38490847cc33459b4ff0e628c
Zhengrong Wang <seanzw@ucla.eduu>
2019-01-26 19:36:41 -0800

    add test case for loop unroller

---
+++ test/loop_unroller/inputs/EmptyLoopBody.c
@@ -0,0 +1,19 @@
+/**
+ * A very simple loop body with no loop-carry dependence.
+ */
+
+#define N 12
+
+int foo(volatile int* a) {
+    int b = 0;
+    for (int i = 0; i < N; ++i) {
+      b = *a;
+    }
+    return b;
+}
+
+int main() {
+    int a = 0;
+    int b = foo(&a);
+    return 0;
+}
