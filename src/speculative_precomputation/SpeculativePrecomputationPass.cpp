60048c59f8421d2af928a49fa862ecefb074fa9e
Zhengrong Wang <seanzw@ucla.edu>
2019-03-09 20:45:53 -0800

    actually allocate the manager

--- src/speculative_precomputation/SpeculativePrecomputationPass.cpp
+++ src/speculative_precomputation/SpeculativePrecomputationPass.cpp
@@ -10,6 +10,8 @@ bool SpeculativePrecomputationPass::initialize(llvm::Module &Module) {
   bool Ret = ReplayTrace::initialize(Module);
 
   this->InstManagerMap.clear();
+  // First initialize all the critical loads.
+  this->initializeCriticalLoadManagers();
 
   return Ret;
 }
@@ -20,8 +22,50 @@ bool SpeculativePrecomputationPass::finalize(llvm::Module &Module) {
 }
 
 void SpeculativePrecomputationPass::transform() {
-  // First initialize all the critical loads.
-  this->initializeCriticalLoadManagers();
+  DEBUG(llvm::errs() << "SpeculativePrecomputation: start.\n");
+
+  while (true) {
+    auto NewDynInstIter = this->Trace->loadOneDynamicInst();
+    llvm::Instruction *NewStaticInst = nullptr;
+
+    if (NewDynInstIter != this->Trace->DynamicInstructionList.end()) {
+      NewStaticInst = (*NewDynInstIter)->getStaticInstruction();
+      assert(NewStaticInst != nullptr && "Invalid static llvm instructions.");
+
+      // Handle the cache warmer.
+      if (Utils::isMemAccessInst(NewStaticInst)) {
+        this->CacheWarmerPtr->addAccess(Utils::getMemAddr(*NewDynInstIter));
+      }
+    } else {
+      // Commit any remaining buffered instructions when hitting the end.
+      while (!this->Trace->DynamicInstructionList.empty()) {
+        this->Serializer->serialize(this->Trace->DynamicInstructionList.front(),
+                                    this->Trace);
+        this->Trace->commitOneDynamicInst();
+      }
+      break;
+    }
+
+    // Try to maintain the size of the buffer the same as retired instruction
+    // buffer (RIB), which is 512.
+    constexpr size_t RIB_SIZE = 512;
+    while (this->Trace->DynamicInstructionList.size() > RIB_SIZE) {
+      this->Serializer->serialize(this->Trace->DynamicInstructionList.front(),
+                                  this->Trace);
+      this->Trace->commitOneDynamicInst();
+    }
+
+    // Check if we found a critical load.
+    if (auto LoadInst = llvm::dyn_cast<llvm::LoadInst>(NewStaticInst)) {
+      auto InstManagerIter = this->InstManagerMap.find(LoadInst);
+      if (InstManagerIter != this->InstManagerMap.end()) {
+        auto &Manager = InstManagerIter->second;
+        Manager.hit(this->Trace);
+      }
+    }
+  }
+
+  DEBUG(llvm::errs() << "SpeculativePrecomputation: done.\n");
 }
 
 void SpeculativePrecomputationPass::dumpStats(std::ostream &O) {}
@@ -40,11 +84,13 @@ void SpeculativePrecomputationPass::initializeCriticalLoadManagers() {
     auto Inst = InstUIDMap.getInst(InstUID);
     auto LoadInst = llvm::dyn_cast<llvm::LoadInst>(Inst);
     assert(LoadInst != nullptr && "Non-Load inst in load profile.");
+
     if (AverageLatency > 40) {
       auto Inserted =
           this->InstManagerMap
-              .emplace(std::piecewise_construct,
-                       std::forward_as_tuple(LoadInst), std::forward_as_tuple())
+              .emplace(
+                  std::piecewise_construct, std::forward_as_tuple(LoadInst),
+                  std::forward_as_tuple(LoadInst, this->OutputExtraFolderPath))
               .second;
       assert(Inserted && "Failed to insert the critical load manager.");
     }
