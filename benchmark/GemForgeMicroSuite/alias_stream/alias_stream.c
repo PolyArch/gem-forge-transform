6557e868b1e88eee9a9acc047392d85e912e6fcb
seanzw <seanyukigeek@gmail.com>
2019-04-28 22:33:55 -0700

    Add a micro suite on stream aliasing

---
+++ benchmark/GemForgeMicroSuite/alias_stream/alias_stream.c
@@ -0,0 +1,20 @@
+// Simple dense vector dot product.
+typedef double Value;
+
+__attribute__((noinline)) Value foo(Value *a, int N) {
+  // Make sure there is no reuse.
+  for (int i = 1; i < N; i += 1) {
+    a[i] += a[i - 1];
+  }
+  return a[N - 1];
+}
+
+// 65536*4 is 512kB.
+const int N = 65536;
+Value a[N];
+
+int main() {
+  volatile Value ret;
+  ret = foo(a, N);
+  return 0;
+}
