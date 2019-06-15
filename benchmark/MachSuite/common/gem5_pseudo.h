8afb11304bcceb299ab0479c2cf6732d989fc54f
Zhengrong Wang <seanzw@ucla.edu>
2019-06-14 17:20:29 -0700

    [misc] Long time ago

---
+++ benchmark/MachSuite/common/gem5_pseudo.h
@@ -0,0 +1,12 @@
+#ifndef VERTICAL_COMMON_H
+#define VERTICAL_COMMON_H
+
+#include "stdint.h"
+void m5_reset_stats(uint64_t ns_delay, uint64_t ns_period);
+void m5_work_begin(uint64_t workid, uint64_t threadid);
+void m5_work_end(uint64_t workid, uint64_t threadid);
+
+#define DETAILED_SIM_START() m5_work_begin(0, 0); m5_reset_stats(0, 0);
+#define DETAILED_SIM_STOP() m5_work_end(0, 0)
+
+#endif
--- benchmark/MachSuite/common/harness.c
+++ benchmark/MachSuite/common/harness.c
@@ -1,84 +1,77 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 //#define WRITE_OUTPUT
 //#define CHECK_OUTPUT
 
 #include "support.h"
 
-#ifdef LLVM_TDG_GEM5_BASELINE
-#include "gem5/m5ops.h"
-#endif
+#include "gem5_pseudo.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   // Parse command line.
   char *in_file;
-  #ifdef CHECK_OUTPUT
+#ifdef CHECK_OUTPUT
   char *check_file;
-  #endif
-  assert( argc<4 && "Usage: ./benchmark <input_file> <check_file>" );
+#endif
+  assert(argc < 4 && "Usage: ./benchmark <input_file> <check_file>");
   in_file = "input.data";
-  #ifdef CHECK_OUTPUT
+#ifdef CHECK_OUTPUT
   check_file = "check.data";
-  #endif
-  if( argc>1 )
+#endif
+  if (argc > 1)
     in_file = argv[1];
-  #ifdef CHECK_OUTPUT
-  if( argc>2 )
+#ifdef CHECK_OUTPUT
+  if (argc > 2)
     check_file = argv[2];
-  #endif
+#endif
 
   // Load input data
   int in_fd;
   char *data;
   data = malloc(INPUT_SIZE);
-  assert( data!=NULL && "Out of memory" );
-  in_fd = open( in_file, O_RDONLY );
-  assert( in_fd>0 && "Couldn't open input data file");
+  assert(data != NULL && "Out of memory");
+  in_fd = open(in_file, O_RDONLY);
+  assert(in_fd > 0 && "Couldn't open input data file");
   input_to_data(in_fd, data);
-  
-  // Unpack and call
-  #ifdef LLVM_TDG_GEM5_BASELINE
-  m5_work_begin(0, 0);
-  m5_reset_stats(0, 0);
-  #endif
-  run_benchmark( data );
-  #ifdef LLVM_TDG_GEM5_BASELINE
-  m5_work_end(0, 0);
-  #endif
 
-  #ifdef WRITE_OUTPUT
+  // Unpack and call
+  DETAILED_SIM_START();
+  run_benchmark(data);
+  DETAILED_SIM_STOP();
+
+#ifdef WRITE_OUTPUT
   int out_fd;
-  out_fd = open("output.data", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
-  assert( out_fd>0 && "Couldn't open output data file" );
+  out_fd = open("output.data", O_WRONLY | O_CREAT | O_TRUNC,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+  assert(out_fd > 0 && "Couldn't open output data file");
   data_to_output(out_fd, data);
   close(out_fd);
-  #endif
+#endif
 
-  // Load check data
-  #ifdef CHECK_OUTPUT
+// Load check data
+#ifdef CHECK_OUTPUT
   int check_fd;
   char *ref;
   ref = malloc(INPUT_SIZE);
-  assert( ref!=NULL && "Out of memory" );
-  check_fd = open( check_file, O_RDONLY );
-  assert( check_fd>0 && "Couldn't open check data file");
+  assert(ref != NULL && "Out of memory");
+  check_fd = open(check_file, O_RDONLY);
+  assert(check_fd > 0 && "Couldn't open check data file");
   output_to_data(check_fd, ref);
-  
-  if( !check_data(data, ref) ) {
+
+  if (!check_data(data, ref)) {
     fprintf(stderr, "Benchmark results are incorrect\n");
     return -1;
   }
   free(data);
   free(ref);
 
-  #endif
+#endif
   printf("Success.\n");
   return 0;
 }
