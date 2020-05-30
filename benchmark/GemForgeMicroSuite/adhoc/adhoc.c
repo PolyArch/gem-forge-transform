3367f5535e94328abd7578fef3655dc7ee5fbfde
seanzw <seanyukigeek@gmail.com>
2020-05-29 19:21:41 -0700

    [misc] Add more omp microbenchmark.

--- benchmark/GemForgeMicroSuite/adhoc/adhoc.c
+++ benchmark/GemForgeMicroSuite/adhoc/adhoc.c
@@ -1,43 +1,50 @@
 
 #include <math.h>
+#include <stdio.h>
 
+#ifdef GEM_FORGE
 #include "gem5/m5ops.h"
+#endif
 
-typedef double Value;
+typedef long long Value;
 
 /**
  * Used for random test purpose.
  */
 
 // 65536*8 is 512kB.
-const int N = 65536 * 2;
+const int N = 128;
 const int STEP_AHEAD = 16;
 Value a[N + STEP_AHEAD * 16];
+#define STRIDE 8
 
 __attribute__((noinline)) Value foo(Value **pa, int N) {
-  Value sum = 0.0f;
   volatile Value *a = *pa;
-  *(double *)a = exp(*a);
-  // Make sure there is no reuse.
-  #pragma nounroll
-  for (long long i = 0; i < N; i += 16) {
-    // Lookahead to trigger IVV (L1_GETS) bug.
-    Value lookahead = a[i + STEP_AHEAD * 16];
-    sum += a[i];
+#pragma nounroll
+  for (long long i = 0; i < N; i += STRIDE) {
+    __atomic_fetch_add(a + i, 1, __ATOMIC_RELAXED);
   }
-  return sum;
+  return 0.0f;
 }
-
 
 int main() {
   volatile Value c;
   Value *pa = a;
   // This should warm up the cache.
-  for (long long i = 0; i < N; i++) {
-    a[i] = 1.32361351;
+  for (long long i = 0; i < N; i += STRIDE) {
+    a[i] = 1;
   }
+#ifdef GEM_FORGE
   m5_detail_sim_start();
+#endif
   c = foo(&pa, N);
+#ifdef GEM_FORGE
   m5_detail_sim_end();
+#endif
+  Value sum = 0.0;
+  for (int i = 0; i < N; i += STRIDE) {
+    sum += a[i];
+  }
+  printf("Ret = %lld, Expected %lld.\n", sum, (Value)((N + N) / STRIDE));
   return 0;
 }
---
+++ benchmark/GemForgeMicroSuite/gfm_graph_utils.c
@@ -0,0 +1,38 @@
+#include "gfm_graph_utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+GraphCSR readInGraph(const char *fn) {
+  GraphCSR graph;
+  FILE *f = fopen(fn, "rb");
+  fread(&graph.numNodes, sizeof(graph.numNodes), 1, f);
+  fread(&graph.numEdges, sizeof(graph.numEdges), 1, f);
+
+  // We make the last pos numEdges so that we can calculate out-degree.
+  graph.edgePtr =
+      aligned_alloc(sizeof(graph.edgePtr[0]),
+                    sizeof(graph.edgePtr[0]) * (graph.numNodes + 1));
+  fread(graph.edgePtr, sizeof(graph.edgePtr[0]), graph.numNodes, f);
+  graph.edgePtr[graph.numNodes] = graph.numEdges;
+
+  graph.edges = aligned_alloc(sizeof(graph.edges[0]),
+                              sizeof(graph.edges[0]) * graph.numEdges);
+  fread(graph.edges, sizeof(graph.edges[0]), graph.numEdges, f);
+  // Sanity check.
+#ifndef GEM_FORGE
+  for (uint64_t i = 0; i < graph.numEdges; ++i) {
+    if (graph.edges[i] >= graph.numNodes) {
+      printf("Illegal %u.\n", graph.edges[i]);
+    }
+  }
+#endif
+  // Read in the root.
+  fread(&graph.root, sizeof(graph.root), 1, f);
+  printf("NumNodes %u NumEdges %u Root %u Pos %luMB EdgePtr %luMB.\n",
+         graph.numNodes, graph.numEdges, graph.root,
+         sizeof(graph.edgePtr[0]) * graph.numNodes / 1024 / 1024,
+         sizeof(graph.edges[0]) * graph.numEdges / 1024 / 1024);
+
+  return graph;
+}
