bc4bf8c60674c3c05a92436cad24c9b22424c9e8
seanzw <seanyukigeek@gmail.com>
2020-07-01 16:41:10 -0700

    [misc] Add o4 and tm ssp config

--- benchmark/GemForgeMicroSuite/stream/array_sum/array_sum.c
+++ benchmark/GemForgeMicroSuite/stream/array_sum/array_sum.c
@@ -1,7 +1,7 @@
 /**
  * Simple array sum.
  */
-#include "gem5/m5ops.h"
+#include "../../gfm_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,12 +13,10 @@ typedef int Value;
 
 __attribute__((noinline)) Value foo(Value *a, int N, int M) {
   Value sum = 0;
-// #pragma clang loop vectorize(disable) unroll(disable)
-//   for (int k = 0; k < M; ++k) {
 #pragma clang loop vectorize(disable) unroll(disable)
-    for (int i = 0; i < N; i += STRIDE) {
-      sum = sum + a[i];
-    }
+  for (int i = 0; i < N; i += STRIDE) {
+    sum = sum + a[i];
+  }
   // }
   return sum;
 }
@@ -30,24 +28,27 @@ Value a[N];
 
 int main() {
 
-  m5_detail_sim_start();
+  gf_detail_sim_start();
 #ifdef WARM_CACHE
   // This should warm up the cache.
   for (long long i = 0; i < N; i++) {
     a[i] = i;
   }
-  m5_reset_stats(0, 0);
+  gf_reset_stats();
 #endif
 
-  volatile Value c = foo(a, N, 10);
-  m5_detail_sim_end();
+  volatile Value computed = foo(a, N, 10);
+  gf_detail_sim_end();
 
 #ifdef CHECK
   Value expected = 0;
   for (int i = 0; i < N; i += STRIDE) {
     expected += a[i];
   }
-  printf("Ret = %d, Expected = %d.\n", c, expected);
+  printf("Computed = %d, Expected = %d.\n", computed, expected);
+  if (computed != expected) {
+    gf_panic();
+  }
 #endif
 
   return 0;
