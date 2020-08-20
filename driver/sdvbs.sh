e1118e1bef94f86f4de9c2a4ec6ce6141f4e835f
seanzw <seanyukigeek@gmail.com>
2020-08-19 22:53:01 -0700

    [misc] Update SDVBS driver.

---
+++ driver/sdvbs.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# rm -f /tmp/job_scheduler.*
+
+# Benchmark=sdvbs.disparity
+# Benchmark=sdvbs.localization
+Benchmark=sdvbs.mser
+# Benchmark=sdvbs.multi_ncut
+# Benchmark=sdvbs.sift
+# Benchmark=sdvbs.stitch
+# Benchmark=sdvbs.svm
+# Benchmark=sdvbs.texture_synthesis
+# Benchmark=sdvbs.tracking
+SimInput=fullhd
+
+# python Driver.py -b $Benchmark --build
+# python Driver.py -b $Benchmark --profile
+# python Driver.py -b $Benchmark --simpoint
+# python Driver.py -b $Benchmark --trace --fake-trace
+
+# python Driver.py -b $Benchmark -t valid.ex -d
+RubyConfig=8x8c
+sim_replay_prefix=replay/ruby/single
+i4=$sim_replay_prefix/i4.tlb.${RubyConfig}
+o4=$sim_replay_prefix/o4.tlb.${RubyConfig}
+o8=$sim_replay_prefix/o8.tlb.${RubyConfig}
+sim_replay=$o8,$o8.bingo-l2pf
+# python Driver.py -b $Benchmark -t valid.ex --sim-input-size $SimInput \
+#     --sim-configs $sim_replay -s &
+    # --gem5-debug SyscallBase
+
+StreamTransform=stream/ex/static/so.store
+# python Driver.py -b $Benchmark -t $StreamTransform -d --transform-debug StreamRegionAnalyzer
+
+run_ssp () {
+    local bench=$1
+    local trans=$2
+    local rubyc=$3
+    local input=$4
+    local threads=$5
+    local parallel=$6
+    local i4=stream/ruby/single/i4.tlb.$rubyc.c
+    local o4=stream/ruby/single/o4.tlb.$rubyc.c-gb-fifo
+    local o8=stream/ruby/single/o8.tlb.$rubyc.c-gb-fifo
+    local o8_link=stream/ruby/single/o8.tlb.$rubyc-link.c
+    # local all_sim=${o8_link}-gb-fifo.flts-mc
+    local all_sim=${o8}
+    python Driver.py -b $bench -t $trans \
+        --sim-configs $all_sim \
+        --sim-input $input \
+        --input-threads $threads \
+        -s -j $parallel \
+        --gem5-debug StreamEngine,StreamBase,StreamAlias --gem5-debug-start 49451008404 | tee mser.log
+        # --gem5-debug RubyStreamLife | tee bfs.log &
+}
+RubyConfig=8x8c
+Threads=1
+Parallel=1
+run_ssp $Benchmark $StreamTransform $RubyConfig $SimInput $Threads $Parallel
