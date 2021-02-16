038ab37c01542f4974868f847601646a9affe3ce
Zhengrong Wang <seanyukigeek@gmail.com>
2021-02-16 13:45:40 -0800

    [fix] Do not unroll for StreamMemIntrinsic as compute stream can not handle it.

--- src/stream/execution/StreamMemIntrinsic.c
+++ src/stream/execution/StreamMemIntrinsic.c
@@ -1,12 +1,12 @@
 void stream_memset(char *dest, char val, long long len) {
-#pragma clang loop vectorize_width(64) unroll_count(8)
+#pragma clang loop vectorize_width(64) unroll(disable) interleave(disable)
   for (long long i = 0; i < len; ++i) {
     dest[i] = val;
   }
 }
 
 void stream_memcpy(char *dest, const char *src, long long len) {
-#pragma clang loop vectorize_width(64) unroll_count(8)
+#pragma clang loop vectorize_width(64) unroll(disable) interleave(disable)
   for (long long i = 0; i < len; ++i) {
     char val = src[i];
     dest[i] = val;
@@ -15,13 +15,13 @@ void stream_memcpy(char *dest, const char *src, long long len) {
 
 void stream_memmove(char *dest, const char *src, long long len) {
   if (dest < src || dest >= src + len) {
-#pragma clang loop vectorize_width(64) unroll_count(8)
+#pragma clang loop vectorize_width(64) unroll(disable) interleave(disable)
     for (long long i = 0; i < len; ++i) {
       char val = src[i];
       dest[i] = val;
     }
   } else if (dest != src) {
-#pragma clang loop vectorize_width(64) unroll_count(8)
+#pragma clang loop vectorize_width(64) unroll(disable) interleave(disable)
     for (long long i = len - 1; i >= 0; --i) {
       char val = src[i];
       dest[i] = val;
